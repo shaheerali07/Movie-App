1abee358e9da775cb72e5f350f489b9ac62cce46
shery <ashaheer606@gmail.com>
2019-10-08 19:37:54 +0500

    Components finished

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../elements/Header/Header";
 import Home from "../Home/Home";
+import Movie from "../Movie/Movie";
+import NotFound from "../elements/NotFound/NotFound";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/:movieID" component={Movie} exact />
+        </switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 };
 export default App;
